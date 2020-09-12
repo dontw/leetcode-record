68286ccc221be40c2cfe23f79ee3767a310c445d
don <lawtrooper@gmail.com>
2020-09-12 19:57:43 +0800

    Complete easy 70

---
+++ 70.climbing-stairs.ts
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=70 lang=typescript
+ *
+ * [70] Climbing Stairs
+ */
+
+// @lc code=start
+function climbStairs(n: number): number {
+    n = n + 1
+    return fib(n)
+};
+
+function fib(n: number): number {
+    let a = 1;
+    let b = 1;
+    for (let i = 3; i <= n; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+// @lc code=end
+
