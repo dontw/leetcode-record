0a7549db2a58c17b820cb846350bdc9f047e3ae5
ChenYu <lawtrooper@gmail.com>
2020-09-14 17:39:14 +0800

    add easy 1518

---
+++ 1518.water-bottles.ts
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1518 lang=typescript
+ *
+ * [1518] Water Bottles
+ */
+
+// @lc code=start
+function numWaterBottles(numBottles: number, numExchange: number): number {
+  let count = 0
+  while(numBottles > numExchange) {
+    
+  }
+};
+// @lc code=end
+
